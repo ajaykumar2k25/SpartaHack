a939e561ba8723fa5d1cd5263806373627cff521
ajaykumar2k25 <96953471+ajaykumar2k25@users.noreply.github.com>
2023-01-28 21:16:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,3 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-df = pd.DataFrame(
-    np.random.randn(1, 2) / [10, 10] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(df)
-
-df = pd.DataFrame(
-    np.random.randn(1, 2) / [50, 50] + [35.76, -12.4],
-    columns=['lat', 'lon'])
-
-st.map(df)
+import leafmap.kepler as leafmap
+m = leafmap.Map(center=[40, -100], zoom=2, height=600, widescreen=False)
+m
